3245d38ed30095ae7cbd1a471d4c5de485df66e2
Nianqing Yao <imbearchild@outlook.com>
2025-12-15 21:14:42 +0800

    Use go 1.25 because of upstream

--- kopia/.make_srpm.sh
+++ kopia/.make_srpm.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Use go 1.25 because of upstream
+dnf -y copr enable @go-sig/golang-rawhide
+
 dnf -y install go-vendor-tools go2rpm python3-specfile
 spectool -g kopia.spec
 go_vendor_archive create ./kopia.spec
