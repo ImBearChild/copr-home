da3fc037877cb576846d4e6a6610d05369d38a43
Nianqing Yao <imbearchild@outlook.com>
2025-12-15 21:27:18 +0800

    add go sig copr

--- kopia/.make_srpm.sh
+++ kopia/.make_srpm.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+#dnf5 install 'dnf5-command(copr)'
 # Use go 1.25 because of upstream
-dnf -y copr enable @go-sig/golang-rawhide
-
+#dnf -y copr enable @go-sig/golang-rawhide
+#ls /etc/yum.repos.d/
+#dnf repolist
+dnf -y --repofrompath='golang,https://download.copr.fedorainfracloud.org/results/@go-sig/golang-rawhide/fedora-$releasever-$basearch/' install golang
 dnf -y install go-vendor-tools go2rpm python3-specfile
 spectool -g kopia.spec
 go_vendor_archive create ./kopia.spec
